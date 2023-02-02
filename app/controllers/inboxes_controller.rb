1d44e0344c462ea6c984ba4c9a5a67b3686c573e
septianmalela <maulanas430@gmail.com>
2023-02-02 15:15:32 +0700

    render with turbo stream file

--- app/controllers/inboxes_controller.rb
+++ app/controllers/inboxes_controller.rb
@@ -2,98 +2,59 @@ class InboxesController < ApplicationController
   before_action :clear_flash_messages
   before_action :set_inbox, only: %i[ show edit update destroy ]
 
-  # GET /inboxes or /inboxes.json
   def index
     @inboxes = Inbox.all
   end
 
-  # GET /inboxes/1 or /inboxes/1.json
   def show
   end
 
-  # GET /inboxes/new
   def new
     @inbox = Inbox.new
   end
 
-  # GET /inboxes/1/edit
   def edit
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.update(@inbox, partial: 'inboxes/form', locals: { inbox: @inbox })
-      end
+      format.turbo_stream
     end
   end
 
-  # POST /inboxes or /inboxes.json
   def create
     @inbox = Inbox.new(inbox_params)
     respond_to do |format|
       if @inbox.save
         flash[:notice] = 'Inbox successfully created.'
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.update('new_inbox', partial: 'inboxes/form', locals: { inbox: Inbox.new }),
-            turbo_stream.append('inboxes', partial: 'inboxes/inbox', locals: { inbox: @inbox }),
-            turbo_stream.update('flash_inbox', partial: 'inboxes/flash')
-          ]
-        end
-        # format.html { redirect_to @inbox }
       else
         flash[:alert] = @inbox.errors.full_messages.to_sentence
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.update('new_inbox', partial: 'inboxes/form', locals: { inbox: @inbox }),
-            turbo_stream.update('flash_inbox', partial: 'inboxes/flash')
-          ]
-        end
-        # format.html { render :new, status: :unprocessable_entity }
       end
+      format.turbo_stream
     end
   end
 
-  # PATCH/PUT /inboxes/1 or /inboxes/1.json
   def update
     respond_to do |format|
       if @inbox.update(inbox_params)
         flash[:notice] = 'Inbox successfully updated.'
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.update(@inbox, partial: 'inboxes/inbox', locals: { inbox: @inbox }),
-            turbo_stream.update('flash_inbox', partial: 'inboxes/flash')
-          ]
-        end
-        # format.html { redirect_to @inbox, notice: "Inbox was successfully updated." }
       else
         flash[:alert] = @inbox.errors.full_messages.to_sentence
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.update(@inbox, partial: 'inboxes/form', locals: { inbox: @inbox }),
-            turbo_stream.update('flash_inbox', partial: 'inboxes/flash')
-          ]
-        end
-        # format.html { render :edit, status: :unprocessable_entity }
       end
+      format.turbo_stream
     end
   end
 
-  # DELETE /inboxes/1 or /inboxes/1.json
   def destroy
-    @inbox.destroy
-    flash[:notice] = 'Inbox successfully deleted.'
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.remove(@inbox),
-          turbo_stream.update('flash_inbox', partial: 'inboxes/flash')
-        ]
+      if @inbox.destroy
+        flash[:notice] = 'Inbox successfully deleted.'
+      else
+        flash[:alert] = @inbox.errors.full_messages.to_sentence
       end
-      # format.html { redirect_to inboxes_url }
+      format.turbo_stream
     end
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
   def clear_flash_messages
     flash.clear
   end
@@ -102,7 +63,6 @@ class InboxesController < ApplicationController
     @inbox = Inbox.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def inbox_params
     params.require(:inbox).permit(:name)
   end
--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,6 +1,5 @@
 class Inbox < ApplicationRecord
   validates :name, presence: true, allow_blank: false
 
-  # after_create_commit { broadcast_prepend_to "inboxes" }
   broadcasts 'inboxes'
 end
